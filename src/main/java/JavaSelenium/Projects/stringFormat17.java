b671e594cb14fa024157c0e55745b53d19acbbec
manojleolappy <manojr1r@gmail.com>
2023-12-09 11:25:05 +0530

    stringFormat

--- src/main/java/JavaSelenium/Projects/stringFormat17.java
+++ src/main/java/JavaSelenium/Projects/stringFormat17.java
@@ -1,2 +1,52 @@
-package JavaSelenium.Projects;public class stringFormat17 {
+package JavaSelenium.Projects;
+
+
+import java.util.Arrays;
+
+public class stringFormat17 {
+    public static void main(String[] args) {
+        String sentence = "this sentence contains mail id amr@gmail.com";
+        printUserName(sentence);
+
+        String sentence1 = "amr1@gmail.com";
+        printUserName(sentence1);
+
+        String sentence2 = "amr2gmail.com";
+        printUserName(sentence2);
+
+        String sentence3 = null;
+        printUserName(sentence3);
+    }
+
+    private static void printUserName(String sentence) {
+        String username = getUserName(sentence);
+        System.out.println(username);
+    }
+
+    private static String sentenceFormatter(String sentence) {
+        String[] words = sentence.split(" ");
+        return Arrays.stream(words)
+                .filter(word -> word.contains("@"))
+                .findFirst()
+                .orElse(null);
+    }
+
+    private static String getUserName(String mailid) {
+        mailid = mailid.trim();
+
+        String extractedMailI;
+
+        if (mailid.contains(" ") && mailid.contains("@")) {
+            extractedMailI = sentenceFormatter(mailid);
+        } else if (mailid.contains("@") && !mailid.contains(" ")) {
+            extractedMailI = mailid;
+        } else {
+            extractedMailI = null;
+        }
+
+        return extractedMailI != null ? extractedMailI.split("@")[0] : null;
+    }
+
 }
+
+
--- src/main/java/JavaSelenium/Projects/stringformat.java
+++ src/main/java/JavaSelenium/Projects/stringformat.java
@@ -1,5 +1,50 @@
 package JavaSelenium.Projects;
 
 public class stringformat {
+    public static void main(String[] args) {
+        String sentence = "this sentence contains mail id amr@gmail.com";
+        String username = getUserName(sentence);
+        System.out.println(username);
 
+        String sentence1 = "amr1@gmail.com";
+        String username1 = getUserName(sentence1);
+        System.out.println(username1);
+
+        String sentence2 = "amr2gmail.com";
+        String username2 = getUserName(sentence2);
+        System.out.println(username2);
+    }
+    
+    public static String sentenceFormatter(String sentence){
+        String mailid = null;
+        String[] words = sentence.split(" ");
+        for (String word : words){
+            if (word.contains("@")){
+                mailid = word;
+
+            }
+        }
+        return mailid;
+
+    }
+
+    public static String getUserName(String mailid){
+        String userName = null;
+        String extractedMailI = null;
+        mailid =mailid.trim();
+        if (mailid != null && mailid.contains(" ") && mailid.contains("@")) {
+            extractedMailI = sentenceFormatter(mailid);
+        }
+        if (mailid != null && mailid.contains("@") && !mailid.contains(" ")) {
+            extractedMailI = mailid;
+        }
+
+        if (extractedMailI != null) {
+            String[] parts = extractedMailI.split("@");
+            if (parts.length == 2) {
+                userName = parts[0];
+            }
+        }
+        return userName;
+    }
 }
